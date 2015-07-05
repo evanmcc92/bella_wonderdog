2e9420ffcc47c0adb05c47c212e51d86d1d0b4c1
Evan McCullough <webmaster@evanamccullough.com>
2015-07-05 15:36:48 -0400

    removed printr

--- success.php
+++ success.php
@@ -25,8 +25,6 @@ $code = $_GET['code'];
 if (isset($code)) {
     // receive OAuth token object
     $data = $instagram->getOAuthToken($code);
-    echo "<pre>";
-    print_r($data);
     $username = $data->user->username;
     // store user access token
     $instagram->setAccessToken($data);
