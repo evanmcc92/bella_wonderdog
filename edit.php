a8abbaf4dd2addbd71a0c63cabc28a0feda2d382
Evan McCullough <webmaster@evanamccullough.com>
2015-07-05 18:05:46 -0400

    removed spaces for tabs, removed unnecessary html from edit

--- edit.php
+++ edit.php
@@ -13,112 +13,105 @@ use MetzWeb\Instagram\Instagram;
 
 // initialize class
 $instagram = new Instagram(array(
-    'apiKey' => 'eda53af4356b4d2d87f9179786ba63dd',
-    'apiSecret' => 'f55117ae3ed24495a6ece2125b6de98f',
-    'apiCallback' => 'https://pacific-journey-4584.herokuapp.com/success.php' // must point to success.php
+	'apiKey' => 'eda53af4356b4d2d87f9179786ba63dd',
+	'apiSecret' => 'f55117ae3ed24495a6ece2125b6de98f',
+	'apiCallback' => 'https://pacific-journey-4584.herokuapp.com/success.php' // must point to success.php
 ));
 // receive OAuth code parameter
 $code = $_SESSION['code'];
 
 // check whether the user has granted access
 if (isset($code)) {
-    // receive OAuth token object
-    $data = $_SESSION['data'];
-    $username = $data->user->username;
-    // store user access token
-    $instagram->setAccessToken($data);
-    // now you have access to all authenticated user methods
-    $result = $instagram->getUserMedia();
+	// receive OAuth token object
+	$data = $_SESSION['data'];
+	$username = $data->user->username;
+	// store user access token
+	$instagram->setAccessToken($data);
+	// now you have access to all authenticated user methods
 } else {
-    // check whether an error occurred
-    if (isset($_SESSION['error'])) {
-        echo 'An error occurred: ' . $_SESSION['error_description'];
-    }
+	// check whether an error occurred
+	if (isset($_SESSION['error'])) {
+		echo 'An error occurred: ' . $_SESSION['error_description'];
+	}
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instagram - photo stream</title>
-    <link href="https://vjs.zencdn.net/4.2/video-js.css" rel="stylesheet">
-    <link href="assets/style.css" rel="stylesheet">
-    <script src="https://vjs.zencdn.net/4.2/video.js"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Instagram - photo stream</title>
+	<link href="https://vjs.zencdn.net/4.2/video-js.css" rel="stylesheet">
+	<link href="assets/style.css" rel="stylesheet">
+	<script src="https://vjs.zencdn.net/4.2/video.js"></script>
 </head>
 <body>
 <div class="container">
-    <header class="clearfix">
-        <img src="assets/instagram.png" alt="Instagram logo">
+	<header class="clearfix">
+		<img src="assets/instagram.png" alt="Instagram logo">
 
-        <h1>Instagram photos <span>taken by <?php echo $data->user->username ?></span></h1>
-    </header>
-    <div class="main">
-        <ul class="grid">
-            <?php
-                $media = $instagram->getMedia($_GET['id']);
-                $content = '<li>';
-                // output media
-                if ($media->type === 'video') {
-                    // video
-                    $poster = $media->images->low_resolution->url;
-                    $source = $media->videos->standard_resolution->url;
-                    $content .= "<video class=\"media video-js vjs-default-skin\" width=\"250\" height=\"250\" poster=\"{$poster}\"
-                           data-setup='{\"controls\":true, \"preload\": \"auto\"}'>
-                             <source src=\"{$source}\" type=\"video/mp4\" />
-                           </video>";
-                } else {
-                    // image
-                    $image = $media->images->low_resolution->url;
-                    $content .= "<img class=\"media\" src=\"{$image}\"/>";
-                }
-                // create meta section
-                $avatar = $media->user->profile_picture;
-                $username = $media->user->username;
-                $comment = $media->caption->text;
-                $id = $media->id;
-                $content .= "<div class=\"content\">
-                           <div class=\"avatar\" style=\"background-image: url({$avatar})\"></div>
-                           <p>{$username}</p>
-                           <div class=\"comment\">{$comment}</div>
-                           <div><a href='edit.php?id=$id'>Edit</a></div>
-                         </div>";
-                foreach ($media->tags as $tags) {
-                    $tagmedia = $instagram->getTagMedia($tag);
-                    echo "<pre>";
-                    print_r($tagmedia);
-                    exit();
-                }
-                // output media
-                echo $content . '</li>';
-            ?>
-        </ul>
-        <!-- GitHub project -->
-        <footer>
-            <p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>,
-                available on GitHub</p>
-            <iframe width="95px" scrolling="0" height="20px" frameborder="0" allowtransparency="true"
-                    src="http://ghbtns.com/github-btn.html?user=cosenary&repo=Instagram-PHP-API&type=fork&count=true"></iframe>
-        </footer>
-    </div>
+		<h1>Instagram photos <span>taken by <?php echo $data->user->username ?></span></h1>
+	</header>
+	<div class="main">
+		<ul class="grid">
+			<?php
+				$media = $instagram->getMedia($_GET['id']);
+				$content = '<li>';
+				// output media
+				if ($media->type === 'video') {
+					// video
+					$poster = $media->images->low_resolution->url;
+					$source = $media->videos->standard_resolution->url;
+					$content .= "<video class=\"media video-js vjs-default-skin\" width=\"250\" height=\"250\" poster=\"{$poster}\"
+						   data-setup='{\"controls\":true, \"preload\": \"auto\"}'>
+							 <source src=\"{$source}\" type=\"video/mp4\" />
+						   </video>";
+				} else {
+					// image
+					$image = $media->images->low_resolution->url;
+					$content .= "<img class=\"media\" src=\"{$image}\"/>";
+				}
+				// create meta section
+				$avatar = $media->user->profile_picture;
+				$username = $media->user->username;
+				$comment = $media->caption->text;
+				$id = $media->id;
+				foreach ($media->tags as $tags) {
+					$tagmedia = $instagram->getTagMedia($tag);
+					echo "<pre>";
+					print_r($tagmedia);
+					exit();
+				}
+				// output media
+				echo $content . '</li>';
+			?>
+		</ul>
+		<!-- GitHub project -->
+		<footer>
+			<p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>,
+				available on GitHub</p>
+			<iframe width="95px" scrolling="0" height="20px" frameborder="0" allowtransparency="true"
+					src="http://ghbtns.com/github-btn.html?user=cosenary&repo=Instagram-PHP-API&type=fork&count=true"></iframe>
+		</footer>
+	</div>
 </div>
 <!-- javascript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>
-    $(document).ready(function () {
-        // rollover effect
-        $('li').hover(
-            function () {
-                var $media = $(this).find('.media');
-                var height = $media.height();
-                $media.stop().animate({marginTop: -(height - 82)}, 1000);
-            }, function () {
-                var $media = $(this).find('.media');
-                $media.stop().animate({marginTop: '0px'}, 1000);
-            }
-        );
-    });
+	$(document).ready(function () {
+		// rollover effect
+		$('li').hover(
+			function () {
+				var $media = $(this).find('.media');
+				var height = $media.height();
+				$media.stop().animate({marginTop: -(height - 82)}, 1000);
+			}, function () {
+				var $media = $(this).find('.media');
+				$media.stop().animate({marginTop: '0px'}, 1000);
+			}
+		);
+	});
 </script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,9 +6,9 @@ use MetzWeb\Instagram\Instagram;
 
 // initialize class
 $instagram = new Instagram(array(
-    'apiKey' => 'eda53af4356b4d2d87f9179786ba63dd',
-    'apiSecret' => 'f55117ae3ed24495a6ece2125b6de98f',
-    'apiCallback' => 'https://pacific-journey-4584.herokuapp.com/success.php' // must point to success.php
+	'apiKey' => 'eda53af4356b4d2d87f9179786ba63dd',
+	'apiSecret' => 'f55117ae3ed24495a6ece2125b6de98f',
+	'apiCallback' => 'https://pacific-journey-4584.herokuapp.com/success.php' // must point to success.php
 ));
 
 // create login URL
@@ -18,38 +18,37 @@ $loginUrl = $instagram->getLoginUrl();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instagram - OAuth Login</title>
-    <link rel="stylesheet" type="text/css" href="assets/style.css">
-    <style>
-        .login {
-            display: block;
-            font-size: 20px;
-            font-weight: bold;
-            margin-top: 50px;
-        }
-    </style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Instagram - OAuth Login</title>
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<style>
+		.login {
+			display: block;
+			font-size: 20px;
+			font-weight: bold;
+			margin-top: 50px;
+		}
+	</style>
 </head>
 <body>
 <div class="container">
-    <header class="clearfix">
-        <h1>Instagram <span>display your photo stream</span></h1>
-    </header>
-    <div class="main">
-        <ul class="grid">
-            <li><img src="assets/instagram-big.png" alt="Instagram logo"></li>
-            <li>
-                <a class="login" href="<?php echo $loginUrl ?>">» Login with Instagram</a>
-                <h4>Use your Instagram account to login.</h4>
-            </li>
-        </ul>
-        <!-- GitHub project -->
-        <footer>
-            <p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>,
-                available on GitHub</p>
-        </footer>
-    </div>
+	<header class="clearfix">
+		<h1>Instagram <span>display your photo stream</span></h1>
+	</header>
+	<div class="main">
+		<ul class="grid">
+			<li><img src="assets/instagram-big.png" alt="Instagram logo"></li>
+			<li>
+				<a class="login" href="<?php echo $loginUrl ?>">» Login with Instagram</a>
+				<h4>Use your Instagram account to login.</h4>
+			</li>
+		</ul>
+		<!-- GitHub project -->
+		<footer>
+			<p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>, available on GitHub</p>
+		</footer>
+	</div>
 </div>
 </body>
 </html>
